800e508c22a6342c96723dd619a19f1194c1a907
Help-Desk23 <vian.sistemas@gmail.com>
2025-03-20 14:57:22 -0400

    Update IP

--- controllers/motos/motos.js
+++ controllers/motos/motos.js
@@ -42,7 +42,7 @@ const addMoto = async (req, res) => {
     const { modelo, precious, inicialbs } = req.body;
 
     const port = req.get('host').split(':')[1];
-    const img_motos = req.file ? `http://192.168.2.88:${port}/uploads/${req.file.filename}` : null;
+    const img_motos = req.file ? `http://192.168.2.242:${port}/uploads/${req.file.filename}` : null;
 
     try{
         const pool = await sql.connect(db);
